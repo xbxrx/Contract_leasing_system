add1012405b560e0951a55db2be6804dc2c0ce04
xbxrx <1152091362@qq.com>
2021-07-29 15:45:11 +0800

    junit数据库单元测试

---
+++ Contract_leasing_system/src/main/java/com/team01/service/IContractInfoService.java
@@ -0,0 +1,26 @@
+package com.team01.service;
+
+
+
+
+import com.team01.domain.ContractInfo;
+
+import java.util.List;
+
+public interface IContractInfoService {
+    public int addContractInfo(ContractInfo contractInfo);
+
+    //根据合同编号删除合同信息
+    public int deleteContractInfo(int contractId);
+
+    //根据合同编号修改合同信息
+    public void updateContractInfo(int contractId);
+
+    //根据合同编号查询合同信息
+    public ContractInfo queryContractInfo(int contractId);
+
+    //查询所有合同信息
+    public List<ContractInfo> queryAllContactInfo();
+
+
+}
